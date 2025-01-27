66e3351502d2b4d10a6e46399b383bac44ff686e
CristianRM05 <crm00423@gmail.com>
2025-01-27 17:07:31 +0100

    agregado validet para usar el token con su caduxidad

--- src/main/java/com/clubnautico/clubnautico/controller/AuthController.java
+++ src/main/java/com/clubnautico/clubnautico/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.clubnautico.clubnautico.controller;
 
+import com.clubnautico.clubnautico.Config.JwtService;
 import com.clubnautico.clubnautico.Service.AauthService;
 import com.clubnautico.clubnautico.controller.Models.AuthResponse;
 import com.clubnautico.clubnautico.controller.Models.AuthenticateRequest;
@@ -7,18 +8,19 @@ import com.clubnautico.clubnautico.controller.Models.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class AuthController {
 @Autowired
  private AauthService authService;
-
+    @Autowired
+    private final JwtService jwtService;
 @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
         return ResponseEntity.ok(authService.register(request));
@@ -27,5 +29,16 @@ public class AuthController {
     @PostMapping("/authenticate")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthenticateRequest request){
         return ResponseEntity.ok(authService.authenticate(request));
+    }
+    @PostMapping("/validate")
+    public ResponseEntity<Boolean> validateToken(@RequestBody Map<String, String> request) {
+        String token = request.get("token");
+        try {
+            // Verifica si el token no ha expirado
+            boolean isValid = !jwtService.isTokenExpired(token);
+            return ResponseEntity.ok(isValid);
+        } catch (Exception e) {
+            return ResponseEntity.ok(false);
+        }
     }
 }
